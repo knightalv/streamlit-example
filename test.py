37c1632d0271a1f6accd404b15047e84db79aa26
knightalv <71338102+knightalv@users.noreply.github.com>
2022-08-21 10:00:48 +0800

    Update test.py

--- test.py
+++ test.py
@@ -10,84 +10,52 @@ import streamlit as st
 st.title("My First App")
 st.write("Hello Amelia!")
 
-import sys
-from pathlib import Path
+import cv2
+import numpy as np
 
-file = Path(__file__).resolve()
-parent, root = file.parent, file.parents[1]
-sys.path.append(str(root))
+def preprocess(img):
+    bytes_data = np.asarray(bytearray(img.read()), dtype=np.uint8)
+    img = cv2.imdecode(bytes_data, cv2.IMREAD_COLOR)
+    return img
 
-try:
-    sys.path.remove(str(parent))
-except ValueError:  # Already removed
-    pass
+def invert(img):
+    img = preprocess(img)
+    inv = cv2.bitwise_not(img)
+    return inv
 
-import streamlit as st
+def hdr(img):
+    img = preprocess(img)
+    hdr_img = cv2.detailEnhance(img, sigma_s=12, sigma_r=0.15)
+    return  hdr_img
 
-VERSION = ".".join(st.__version__.split(".")[:2])
+def sketch(img):
+    img = preprocess(img)
+    _, sketch_img = cv2.pencilSketch(img, sigma_s=60, sigma_r=0.07, shade_factor=0.1) 
+    return  sketch_img
 
-from demos import orchestrator
+def gray(img):
+    img = preprocess(img)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray_img=cv2.cvtColor(gray_img, cv2.COLOR_GRAY2RGB)
+    return gray_img
 
-demo_pages = {
-    "st.camera_input": orchestrator.show_examples,
+def none(img):
+    img = preprocess(img)
+    return img
+
+picture = st.camera_input("First, take a picture...")
+
+filters_to_funcs = {
+    "No filter": none,
+    "Invert": invert,
+    "HDR": hdr,
+    "Sketch": sketch,
+    "Grayscale": gray
 }
+filters = st.selectbox("...and now, apply a filter!", filters_to_funcs.keys())
 
-
-
-contributors = []
-
-intro = f"""
-This release launches Camera Input as well as other improvements and bug fixes.
-"""
-
-release_notes = f"""
----
-**Highlights**
-- 📸 Introducing `st.camera_input` for uploading images straight from your camera.
-**Notable Changes**
-- 🚦 Widgets now have the `disabled` parameter that removes interactivity.
-- 🚮 Clear `st.experimental_memo` and `st.experimental_singleton` programmatically by using the `clear()` method on a cached function.
-- 📨 Developers can now configure the maximum size of a message to accommodate larger messages within the Streamlit application. See `server.maxMessageSize`.
-- 🐍 We formally added support for Python 3.10.
-**Other Changes**
-- 😵‍💫 Calling `str` or `repr` on `threading.current_thread()` does not cause a RecursionError ([#4172](https://github.com/streamlit/streamlit/issues/4172)).
-- 📹 Gracefully stop screencast recording when user removes permission to record ([#4180](https://github.com/streamlit/streamlit/pull/4180)).
-- 🌇 Better scale images by using a higher-quality image bilinear resampling algorithm ([#4159](https://github.com/streamlit/streamlit/pull/4159)).
-[Click here](https://github.com/streamlit/streamlit/compare/1.3.0...1.4.0) to check out all updates. As always, thank you to all [our contributors](https://github.com/streamlit/streamlit/graphs/contributors) who help make Streamlit awesome!
-"""
-
-# End release updates
-
-
-def draw_main_page():
-    st.write(
-        f"""
-        # Welcome to Streamlit 1.4.0! 👋
-        """
-    )
-
-    st.write(intro)
-
-    st.write(release_notes)
-
-
-# Draw sidebar
-pages = list(demo_pages.keys())
-
-if len(pages):
-    pages.insert(0, "Release Notes")
-    st.sidebar.title(f"Streamlit v1.4.0 🎈")
-    query_params = st.experimental_get_query_params()
-    if "page" in query_params and query_params["page"][0] == "headliner":
-        index = 1
-    else:
-        index = 0
-    selected_demo = st.sidebar.radio("", pages, index, key="pages")
+if picture:
+    img = st.image(filters_to_funcs[filters](picture), channels="BGR")
+    st.write(img)
 else:
-    selected_demo = "Release Notes"
-
-# Draw main page
-if selected_demo in demo_pages:
-    demo_pages[selected_demo]()
-else:
-    draw_main_page()
+    st.write("No Image Available")
